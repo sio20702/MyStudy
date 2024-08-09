3c6aa5c84035d050fea7233568fe2ce9d0d16199
sio20702 <sjshin040702@gmail.com>
2024-08-09 18:36:22 +0900

    fix Test3

--- src/test/Test3.java
+++ src/test/Test3.java
@@ -21,6 +21,7 @@ public class Test3
 		System.out.println("\nEscape sequences practice: ");
 		System.out.println("\\n : "); System.out.println("Hello\nWorld");
 		System.out.println("\\r : "); System.out.println("Hello\rWorld");
+		System.out.println("System.lineSeparator() : "); System.out.println("Hello" + System.lineSeparator() + "World");
 		System.out.println("\\t : "); System.out.println("Hello\tWorld");
 		System.out.println("\\b : "); System.out.println("Hel\bloWorld");
 		
@@ -31,11 +32,15 @@ public class Test3
 		System.out.print("sqrt: "); System.out.println(Math.sqrt(a)+", "+Math.sqrt(b));
 		System.out.print("abs: "); System.out.println(Math.abs(c));
 		System.out.print("random1: "); System.out.println(Math.random());
-		System.out.print("random2: "); System.out.println((int)Math.random()*101);
+		System.out.print("random2: "); System.out.println((int)(Math.random()*101));
 		
 		Random random=new Random(); //create obj for class Random
 		int randomN=random.nextInt(10);
-		System.out.print("\nrealRandom: "); System.out.println(randomN);
+		System.out.print("\npseudo Random: "); System.out.println(randomN);
+		
+		random = new Random(1);
+		System.out.print("\npseudo Random with seed 1: "); System.out.println(random.nextInt(10));
+		
 	
 		System.out.print("\nTesting boolean expression: \n");
 		System.out.println(10<9);
